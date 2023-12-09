f02cb3dfd2accc0f0496f3a6ac128a90b109e12e
David_Holliday <dhholliday@icloud.com>
2023-12-09 14:33:22 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'daves-expenses-4',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
